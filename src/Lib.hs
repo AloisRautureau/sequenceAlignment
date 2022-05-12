249dacfee62e805228a2adc79e46443ebeea5116
Aloïs Rautureau <alois.rautureau@gmail.com>
2022-05-13 01:40:05 +0200

    Using actual dynamic programming instead of sugarcoated recursion

--- src/Lib.hs
+++ src/Lib.hs
@@ -4,6 +4,8 @@ import Data.Array
 
 type Sequence = String
 
+-- naive recursive version
+
 hammingDistance :: Eq a => [a] -> [a] -> Int
 hammingDistance x y = sum [1 | (x', y') <- x `zip` y, x' /= y']
 
@@ -15,44 +17,49 @@ alignRecursive seq1@(x:xs) seq2@(y:ys) = head $ sortBy alignementQuality possibi
           alignementQuality (a1, a2) (b1, b2) = compare (hammingDistance a1 a2) (hammingDistance b1 b2)
           fuse (x, y) (x', y') = (x:x', y:y')
 
+-- Needleman-Wunsch algorithm
+
+-- This could have been a similarity matrix, but it felt like forcibly added
+-- complexity. This is a basic implementation after all.
+type Cost = Int
 matchCost = 3
 mismatchCost = -1
 indelCost = -2
 
-data Alignement = Match | DelX | DelY | Mismatch | Init deriving (Eq, Show)
+data Alignement = Match | Mismatch | Up | Left deriving (Eq, Show, Ord)
+type Path = [Alignement]
 
-needlemanWunschPath :: Sequence -> Sequence -> [Alignement]
-needlemanWunschPath seq1 seq2 = reverse $ pathFrom (length seq1) (length seq2)
-    where cell 0 0 = (0, Init)
-          cell i 0 = (i * indelCost, DelX)
-          cell 0 j = (j * indelCost, DelY)
-          cell i j = head $ sortBy (\(a, _) (b, _) -> compare b a) possibilities 
-            where possibilities = [
-                    (fst (cell (i-1) j) + indelCost, DelX),
-                    (fst (cell i (j-1)) + indelCost, DelY),
-                    (fst (cell (i-1) (j-1)) + matchScore i j, matchStatus i j)
-                    ]
-                  max3 a b = max a . max b
-                  charAt n = last . take (n+1)
-                  matchStatus i j | charAt (i-1) seq1 == charAt (j-1) seq2 = Match | otherwise = Mismatch
-                  matchScore i j | matchStatus i j == Match = matchCost | otherwise = mismatchCost
-          pathFrom i j = 
-            let direction = snd (cell i j) 
-            in direction : case direction of
-                DelX -> pathFrom (i-1) j
-                DelY -> pathFrom i (j-1)
-                Match -> pathFrom (i-1) (j-1)
-                Mismatch -> pathFrom (i-1) (j-1)
-                Init -> []
+-- Given two sequences, returns a cost and an optimal path following
+-- the Needleman-Wunsch algorithm
+needlemanWunschPath :: Sequence -> Sequence -> Path
+needlemanWunschPath seq1 seq2 = snd $ matrix ! (length seq2) ! (length seq1)
+    where matrix = listArray (0, length seq2) [row i | i <- [0..length seq2]]
+          -- In order to compute the value of a cell, we only need the row
+          -- above it, and the left neighboring cell.
+          -- Therefore, we only need to initialize first row, and first cell
+          -- of each row after that.
+          row 0 = listArray (0, length seq1) [(j * indelCost, replicate j Lib.Left) | j <- [0..length seq1]]
+          row i = listArray (0, length seq1) $ (i * indelCost, replicate i Up) : [cell i j | j <- [1..length seq1]]
+          cell i j = head $ sortBy scoreSort [
+                update (matrix ! (i-1) ! (j-1)) (score i j), 
+                update (matrix ! (i-1) ! j) (indelCost, Lib.Up),
+                update (matrix ! i ! (j-1)) (indelCost, Lib.Left)
+            ]
+          score i j | (seq1 !! (j-1)) == (seq2 !! (i-1)) = (matchCost, Match) | otherwise = (mismatchCost, Mismatch)
+          -- We sort possible move given possible score, and prioritize matches
+          -- when the possible scores are equal.
+          scoreSort (s,m) (s',m')
+            | s == s' = compare m m'
+            | otherwise = compare s' s
+          update (score, path) (cost, move) = (score + cost, path ++ [move])
 
+-- Align from a sequence of moves in the Needleman-Wunsch matrix
 align :: Sequence -> Sequence -> (Sequence, Sequence)
-align seq1 seq2 = _align seq1 seq2 (needlemanWunschPath seq1 seq2)
-    where _align (x:xs) (y:ys) (a:as) = case a of
-                                            Match -> fuse (x, y) (_align xs ys as)
-                                            Mismatch -> fuse (x, y) (_align xs ys as)
-                                            DelY -> fuse ('-', y) (_align (x:xs) ys as)
-                                            DelX -> fuse (x, '-') (_align xs (y:ys) as)
-                                            Init -> _align (x:xs) (y:ys) as
-          _align [] seq2 _ = alignRecursive [] seq2
-          _align seq1 [] _ = alignRecursive seq1 []
-          fuse (x, y) (x', y') = (x:x', y:y')
+align seq1 seq2 = withAlignement seq1 seq2 (needlemanWunschPath seq1 seq2)
+    where withAlignement (x:xs) (y:ys) (alignment:as)
+            | alignment == Up = appendTuple ('-', y) (withAlignement (x:xs) ys as)
+            | alignment == Lib.Left = appendTuple (x, '-') (withAlignement xs (y:ys) as)
+            | otherwise = appendTuple (x, y) (withAlignement xs ys as)
+          withAlignement [] s _ = (replicate (length s) '-', s)
+          withAlignement s [] _ = (s, replicate (length s) '-')
+          appendTuple (a, b) (la, lb) = (a:la,b:lb)
