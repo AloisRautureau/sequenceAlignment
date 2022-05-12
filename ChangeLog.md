ea54913f967f069cf057d8a16893b85908613b67
Aloïs Rautureau <alois.rautureau@gmail.com>
2022-05-13 01:56:59 +0200

    Some cleanup and adding a main function

--- ChangeLog.md
+++
@@ -1,3 +0,0 @@
-# Changelog for sequenceAlignement
-
-## Unreleased changes
--- app/Main.hs
+++ app/Main.hs
@@ -1,7 +1,12 @@
 module Main where
 
 import Lib
+import System.Environment
 
 main :: IO ()
 main = do
-    putStrLn "aled"
+    (seq1:seq2:_) <- getArgs
+    let (aligned1, aligned2) = align seq1 seq2
+    putStrLn "Result:"
+    putStrLn aligned1
+    putStrLn aligned2
