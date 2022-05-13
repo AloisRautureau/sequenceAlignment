05d55d34ee93b2d6944281e4276a5a55cfb5ed86
Aloïs Rautureau <alois.rautureau@gmail.com>
2022-05-13 12:56:19 +0200

    Functional local alignment algorithm, does not display well for now

--- app/Main.hs
+++ app/Main.hs
@@ -6,7 +6,12 @@ import System.Environment
 main :: IO ()
 main = do
     (seq1:seq2:_) <- getArgs
-    let (aligned1, aligned2) = align seq1 seq2
-    putStrLn "Result:"
-    putStrLn aligned1
-    putStrLn aligned2
+    let (galigned1, galigned2) = globalAlign seq1 seq2
+    let (laligned1, laligned, laligned2) = localAlign seq1 seq2
+    putStrLn "Global alignment:"
+    putStrLn galigned1
+    putStrLn galigned2
+    putStrLn "Local alignment:"
+    putStrLn laligned1
+    putStrLn laligned
+    putStrLn laligned2
--- lib/Lib.hs
+++ lib/Lib.hs
@@ -1,4 +1,4 @@
-module Lib (alignRecursive, align) where
+module Lib (alignRecursive, globalAlign, localAlign) where
 import Data.List (sortBy)
 import Data.Array
 
@@ -22,17 +22,17 @@ alignRecursive seq1@(x:xs) seq2@(y:ys) = head $ sortBy alignementQuality possibi
 -- This could have been a similarity matrix, but it felt like forcibly added
 -- complexity. This is a basic implementation after all.
 type Cost = Int
-matchCost = 3
+matchCost = 1
 mismatchCost = -1
 indelCost = -2
 
 data Alignement = Match | Mismatch | Up | Left deriving (Eq, Show, Ord)
 type Path = [Alignement]
 
--- Given two sequences, returns a cost and an optimal path following
+-- Given two sequences, returns an optimal path following
 -- the Needleman-Wunsch algorithm
-needlemanWunschPath :: Sequence -> Sequence -> Path
-needlemanWunschPath seq1 seq2 = snd $ matrix ! (length seq2) ! (length seq1)
+needlemanWunschAlignment :: Sequence -> Sequence -> Path
+needlemanWunschAlignment seq1 seq2 = snd $ matrix ! (length seq2) ! (length seq1)
     where matrix = listArray (0, length seq2) [row i | i <- [0..length seq2]]
           -- In order to compute the value of a cell, we only need the row
           -- above it, and the left neighboring cell.
@@ -53,13 +53,45 @@ needlemanWunschPath seq1 seq2 = snd $ matrix ! (length seq2) ! (length seq1)
             | otherwise = compare s' s
           update (score, path) (cost, move) = (score + cost, path ++ [move])
 
--- Align from a sequence of moves in the Needleman-Wunsch matrix
-align :: Sequence -> Sequence -> (Sequence, Sequence)
-align seq1 seq2 = withAlignement seq1 seq2 (needlemanWunschPath seq1 seq2)
-    where withAlignement (x:xs) (y:ys) (alignment:as)
-            | alignment == Up = appendTuple ('-', y) (withAlignement (x:xs) ys as)
-            | alignment == Lib.Left = appendTuple (x, '-') (withAlignement xs (y:ys) as)
-            | otherwise = appendTuple (x, y) (withAlignement xs ys as)
-          withAlignement [] s _ = (replicate (length s) '-', s)
-          withAlignement s [] _ = (s, replicate (length s) '-')
-          appendTuple (a, b) (la, lb) = (a:la,b:lb)
+-- Given two sequences, returns an optimal local alignment using the
+-- Smith-Waterman algorithm
+smithWatermanAlignment :: Sequence -> Sequence -> (Path, (Int, Int))
+smithWatermanAlignment seq1 seq2 = ((snd $ matrix ! maxCellI ! maxCellJ), maxCellIx)
+    where matrix = listArray (0, length seq2) [row i | i <- [0..length seq2]]
+          row 0 = listArray (0, length seq1) (replicate (length seq1 + 1) (0, []))
+          row i = listArray (0, length seq1) $ (0, []) : [cell i j | j <- [1..length seq1]]
+          cell i j = head $ sortBy scoreSort [
+                update (matrix ! (i-1) ! (j-1)) (score i j),
+                update (matrix ! (i-1) ! j) (indelCost, Lib.Up),
+                update (matrix ! i ! (j-1)) (indelCost, Lib.Left),
+                (0, [])
+            ]
+          score i j | (seq1 !! (j-1)) == (seq2 !! (i-1)) = (matchCost, Match) | otherwise = (mismatchCost, Mismatch)
+          scoreSort (s, m) (s', m')
+            | s == s' = compare m m'
+            | otherwise = compare s' s
+          update (score, path) (cost, move) = (score + cost, path ++ [move])
+          maxCellIx@(maxCellI, maxCellJ) = fst $ foldl1 f [((i, j), cell) | i <- [0..length seq2], let (j, cell) = maxCellRow i]
+            where f x@(_, a) y@(_, b) | scoreSort a b == LT = x | otherwise = y
+                  maxCellRow i = foldl1 f (assocs (matrix ! i))
+
+-- Functions returning the aligned sequences
+traceback :: Sequence -> Sequence -> Path -> Char -> (Sequence, Sequence)
+traceback seq1@(x:xs) seq2@(y:ys) (a:as) c
+    | a == Up = appendTuple (c, y) (traceback seq1 ys as c)
+    | a == Lib.Left = appendTuple (x, c) (traceback xs seq2 as c)
+    | otherwise = appendTuple (x, y) (traceback xs ys as c)
+    where appendTuple (a, b) (la, lb) = (a:la, b:lb)
+traceback [] s _ c = (replicate (length s) c, s)
+traceback s [] _ c = (s, replicate (length s) c)
+
+globalAlign :: Sequence -> Sequence -> (Sequence, Sequence)
+globalAlign seq1 seq2 = traceback seq1 seq2 (needlemanWunschAlignment seq1 seq2) '-'
+
+localAlign :: Sequence -> Sequence -> (Sequence, Sequence, Sequence)
+localAlign seq1 seq2 = (seq1'', reverse seq2'', alignedSeq2)
+    where seq1' = drop (length seq1 - ixSeq1) seq1
+          seq2' = reverse $ take ixSeq2 seq2
+          (path, (ixSeq2, ixSeq1)) = smithWatermanAlignment seq1 seq2
+          (seq1'', seq2'') = traceback seq1' seq2' path ' '
+          alignedSeq2 = replicate (length seq1 - length seq2') ' ' ++ seq2
